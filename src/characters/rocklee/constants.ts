c3f54efad506e7a817fe654f0988d6314790b6d2
Justin Chan <justin.the.c@gmail.com>
2025-12-31 09:30:10 +0000

    Update constants

--- src/characters/rocklee/constants.ts
+++ src/characters/rocklee/constants.ts
@@ -2,10 +2,10 @@ export const ROCK_LEE_CONSTANTS = {
     // Q: Leaf Hurricane
     LEAF_HURRICANE: {
         COOLDOWN: 5 * 60,
-        DURATION: 150, // Increased to 150 (2.5 seconds)
+        DURATION: 250,
         RADIUS: 80,
-        MIN_DAMAGE: 5,
-        MAX_DAMAGE: 20,
+        MIN_DAMAGE: 1,
+        MAX_DAMAGE: 5,
         TICK_RATE: 5
     },
 
@@ -17,14 +17,14 @@ export const ROCK_LEE_CONSTANTS = {
 
     // Space: Dash
     DASH: {
-        COOLDOWN: 100,
+        COOLDOWN: 150,
         MAX_CHARGES: 2,
         DURATION: 8,
         SPEED: 15,
     },
 
     STATS: {
-        HP: 140,
-        SPEED: 3.5,
+        HP: 130,
+        SPEED: 3.25,
     }
 };
